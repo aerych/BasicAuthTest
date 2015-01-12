4331abc76b0f37bbabac1022ee52949f867b4ef0
aerych <aerychj@gmail.com>
2015-01-12 08:54:50 -0600

    Sample Test

--- BasicAuthTest/ViewController.m
+++ BasicAuthTest/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <NSURLConnectionDelegate>
 
 @end
 
@@ -17,11 +17,54 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button setTitle:@"Test" forState:UIControlStateNormal];
+    button.frame = CGRectMake(100, 100, 200, 40);
+    [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(perform) forControlEvents:UIControlEventTouchUpInside];
+
+    [self.view addSubview:button];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)perform
+{
+NSLog(@"PERFORM!!");
+    NSInteger count = 6;
+    for (NSInteger i = 0; i < count; i++) {
+        [self doRequest];
+    }
 }
+
+- (void)doRequest
+{
+NSLog(@"do request");
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://bh.wpmt.co/xmlrpc.php"]];
+    [NSURLConnection connectionWithRequest:request delegate:self];
+}
+
+- (void)connection:(NSURLConnection *)connection willSendRequestForAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge
+{
+    NSString *userName = @"";
+    NSString *password = @"";
+    NSURLCredential *credential = [NSURLCredential credentialWithUser:userName password:password persistence:NSURLCredentialPersistenceForSession];
+    if ([challenge previousFailureCount] == 0 && credential) {
+NSLog(@"responding to request");
+        [[challenge sender] useCredential:credential forAuthenticationChallenge:challenge];
+    } else {
+NSLog(@"!!! Failed to auth, this is an extra request");
+    }
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    NSLog(@"finished");
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    NSLog(@"Connection failed with error: %@", error);
+}
+
 
 @end
