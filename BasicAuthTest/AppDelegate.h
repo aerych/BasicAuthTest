ecc241fa60f0ff47e002c446a1433d1db6d11336
aerych <aerychj@gmail.com>
2015-01-11 18:43:46 -0600

    Initial Commit

---
+++ BasicAuthTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BasicAuthTest
+//
+//  Created by aerych on 1/11/15.
+//  Copyright (c) 2015 Aerych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BasicAuthTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BasicAuthTest
+//
+//  Created by aerych on 1/11/15.
+//  Copyright (c) 2015 Aerych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
